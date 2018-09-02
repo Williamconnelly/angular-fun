d486f93b773f582aac2884165d27a76a260bed6e
William Connelly <williamconnelly.dev@gmail.com>
2018-09-01 19:55:04 -0700

    Integrated new service and refactored the posting process

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,22 +1,30 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // Instead of importing service and including in app.module providers
 @Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   getPosts() {
     // Takes all elements of another array and adds them to the new array. Copying the original array.
     return [...this.posts];
   }
 
-  addPosts(title: string, content: string) {
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
     const post: Post = {
       title,
       content
     };
     this.posts.push(post);
+    // Emmits the copy of the posts.
+    this.postsUpdated.next([...this.posts]);
   }
 
 }
